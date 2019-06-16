90575829bd170c0913528ec82133876f13a9d74a
moriciogithub <moricio@gmail.com>
2019-06-16 10:20:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-ykmbtr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-ykmbtr)
---
+++ pages/prueba/prueba.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-prueba',
+  templateUrl: 'prueba.html'
+})
+export class PruebaPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
